27f5017747939af4ff3dff4853eace1cfb7fe34b
CJ McCaskill <cjmccaskill@gmail.com>
2021-09-15 13:30:23 -0500

    add utils folder

---
+++ utils/api.js
@@ -0,0 +1,33 @@
+export function getStrapiURL(path) {
+  return `${
+    process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337"
+  }${path}`;
+}
+
+// Helper to make GET requests to Strapi
+export async function fetchAPI(path) {
+  const requestUrl = getStrapiURL(path);
+  const response = await fetch(requestUrl);
+  const data = await response.json();
+  return data;
+}
+
+export async function getCategories() {
+  const categories = await fetchAPI("/categories");
+  return categories;
+}
+
+export async function getCategory(slug) {
+  const categories = await fetchAPI(`/categories?slug=${slug}`);
+  return categories?.[0];
+}
+
+export async function getAutos() {
+  const autos = await fetchAPI("/autos");
+  return autos;
+}
+
+export async function getAuto(slug) {
+  const autos = await fetchAPI(`/auto?slug=${slug}`);
+  return autos?.[0];
+}
