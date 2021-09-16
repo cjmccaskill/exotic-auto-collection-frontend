62e86d93395cf14a1df7e13272078e509080b11a
CJ McCaskill <cjmccaskill@gmail.com>
2021-09-15 22:57:47 -0500

    styled header title

--- components/CategoryButtons.js
+++ components/CategoryButtons.js
@@ -4,13 +4,13 @@ export default function CategoryButtons({ categories = [] }) {
   return (
     <div className=" container flex justify-center flex-wrap mx-auto gap-2 mt-4">
       <Link href={"/"}>
-        <a className="bg-white focus:outline-none focus:bg-green-400 focus:text-gray-800 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-xl hover:shadow-lg shadow-md transform hover:translate-x-0.5 hover:translate-y-0.5 transition-200">
+        <a className="bg-white focus:outline-none focus:bg-green-400 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-xl hover:shadow-lg shadow-md transform hover:translate-x-0.5 hover:translate-y-0.5 transition-200">
           All
         </a>
       </Link>
       {categories.map((category) => (
         <Link href={`/categories/${category.slug}`} key={category.id}>
-          <a className=" bg-white focus:outline-none focus:bg-green-400 focus:text-gray-800 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-xl hover:shadow-lg shadow-md transform hover:translate-x-0.5 hover:translate-y-0.5 transition-200">
+          <a className=" bg-white focus:outline-none focus:bg-green-400 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-xl hover:shadow-lg shadow-md transform hover:translate-x-0.5 hover:translate-y-0.5 transition-200">
             {category.slug}
           </a>
         </Link>
--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -6,7 +6,10 @@ export default function SiteHeader() {
     <nav className="bg-black py-3 px-20 lg:flex space-y-3 lg:space-y-0 text-center lg:text-left text-gray-300 justify-between">
       {/* logo on the left side of nav */}
       <Link href="/">
-        <a href="" className="font-extrabold text-xl text-white hover:text-green-400">
+        <a
+          href=""
+          className="font-extrabold text-xl text-white hover:text-green-400 hover:underline"
+        >
           Exotic Auto Collection
         </a>
       </Link>
@@ -14,9 +17,7 @@ export default function SiteHeader() {
         {/* right side of nav nav-link */}
         <UploadModal />
         {/* The button that will open shopping cart */}
-        <button
-          className="snipcart-checkout ml-2 -mr-8 py-1 px-4 bg-white text-gray-700 rounded-full border-transparent border-2 hover:border-green-500 hover:shadow-inner focus:outline-none focus:bg-green-400 focus:text-gray-800"
-        >
+        <button className="snipcart-checkout ml-2 -mr-8 py-1 px-4 bg-white text-gray-700 rounded-full border-transparent border-2 hover:border-green-500 hover:shadow-inner focus:outline-none focus:bg-green-400 focus:text-gray-800">
           Purchase
         </button>
       </div>
