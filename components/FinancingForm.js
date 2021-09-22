bbc5a3bc6b3b6192f893a3346d4a899580168fa5
CJ McCaskill <cjmccaskill@gmail.com>
2021-09-22 15:37:34 -0500

    add addition inputs on form

--- components/FinancingForm.js
+++ components/FinancingForm.js
@@ -12,9 +12,10 @@ export default function FinanceForm() {
         <div className="border-b-2 border-black w-32 mx-auto mt-0 mb-5">
           &nbsp;
         </div>
-        <p>~ 
-          Thank you for trusting{" "}
-          <span className="font-extrabold">Exotic Auto Collection</span> with all your vehicle needs. ~
+        <p>
+          ~ Thank you for trusting
+          <span className="font-extrabold">Exotic Auto Collection</span> with
+          all your vehicle needs. ~
         </p>
       </div>
     );
@@ -29,7 +30,7 @@ export default function FinanceForm() {
         apply for financing
       </div>
       <div className="text-lg font-semibold border-b-2 border-gray-300 mb-4">
-        Let&apos;s get started{" "}
+        Let&apos;s get started
         <span className="text-xs font-extralight">* Required Fields</span>
       </div>
       <div>
@@ -74,7 +75,7 @@ export default function FinanceForm() {
         />
       </div>
       <div>
-        <label>Zip Code</label>
+        <label>Zip Code*</label>
         <input
           type="text"
           name="zipcode"
@@ -82,6 +83,74 @@ export default function FinanceForm() {
           className="form-control cursor-pointer pl-2 mt-2 mb-2 text-sm h-8 rounded-md w-full"
         />
       </div>
+      <legend>Locale*</legend>
+      <div className="flex justify-between flex-wrap mt-2 sm:mt-0">
+        <input type="text" name="city" placeholder="City" required="" className="w-full sm:w-4/6 form-control cursor-pointer pl-2 mt-2 mb-2 text-sm h-8 rounded-md"/>
+        <select name="state" required="" className="form-control cursor-pointer pl-2 mt-2 mb-2 text-sm h-8 rounded-md">
+          <option value="Select" selected="" disabled="">
+            State
+          </option>
+          <option value="AL">Alabama</option>
+          <option value="AK">Alaska</option>
+          <option value="AZ">Arizona</option>
+          <option value="AR">Arkansas</option>
+          <option value="CA">California</option>
+          <option value="CO">Colorado</option>
+          <option value="CT">Connecticut</option>
+          <option value="DE">Delaware</option>
+          <option value="DC">District Of Columbia</option>
+          <option value="FL">Florida</option>
+          <option value="GA">Georgia</option>
+          <option value="HI">Hawaii</option>
+          <option value="ID">Idaho</option>
+          <option value="IL">Illinois</option>
+          <option value="IN">Indiana</option>
+          <option value="IA">Iowa</option>
+          <option value="KS">Kansas</option>
+          <option value="KY">Kentucky</option>
+          <option value="LA">Louisiana</option>
+          <option value="ME">Maine</option>
+          <option value="MD">Maryland</option>
+          <option value="MA">Massachusetts</option>
+          <option value="MI">Michigan</option>
+          <option value="MN">Minnesota</option>
+          <option value="MS">Mississippi</option>
+          <option value="MO">Missouri</option>
+          <option value="MT">Montana</option>
+          <option value="NE">Nebraska</option>
+          <option value="NV">Nevada</option>
+          <option value="NH">New Hampshire</option>
+          <option value="NJ">New Jersey</option>
+          <option value="NM">New Mexico</option>
+          <option value="NY">New York</option>
+          <option value="NC">North Carolina</option>
+          <option value="ND">North Dakota</option>
+          <option value="OH">Ohio</option>
+          <option value="OK">Oklahoma</option>
+          <option value="OR">Oregon</option>
+          <option value="PA">Pennsylvania</option>
+          <option value="RI">Rhode Island</option>
+          <option value="SC">South Carolina</option>
+          <option value="SD">South Dakota</option>
+          <option value="TN">Tennessee</option>
+          <option value="TX">Texas</option>
+          <option value="UT">Utah</option>
+          <option value="VT">Vermont</option>
+          <option value="VA">Virginia</option>
+          <option value="WA">Washington</option>
+          <option value="WV">West Virginia</option>
+          <option value="WI">Wisconsin</option>
+          <option value="WY">Wyoming</option>
+        </select>
+      </div>
+      <label>Note</label>
+      <textarea
+        rows="4"
+        name="note"
+        id="note"
+        placeholder="Include any additional information"
+        className="form-control cursor-pointer pl-2 mt-2 pt-1 mb-2 text-sm rounded-md w-full"
+      ></textarea>
       {/* submit button */}
       <button
         type="submit"
